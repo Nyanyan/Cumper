2f0b7c4344da26e4ea930574133df6bafdff9922
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-11-03 23:08:35 +0900

    not-working

--- PullDataFromWCA.py
+++ PullDataFromWCA.py
@@ -6,6 +6,11 @@ url = 'https://www.worldcubeassociation.org/persons/'+wcaId
 numOfComps = 0
 bestAverage3 = 0
 
-dfs = pd.read_html(url)
-print(dfs[1][['Average']].head())
-print(len(dfs))
+df = pd.read_html(url)
+print(df[1])
+print(df[1][['Average']].head())
+
+df2 = pd.DataFrame(data=df[1], index=['3x3x3 Cube'], columns=['Average'])
+print(df2)
+bestAverage3 = df2.loc['3x3x3 Cube']['Average']
+print(bestAverage3)
