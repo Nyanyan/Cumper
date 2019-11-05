f302927f912e369fb3112646cc7308fcf991880e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-11-05 18:07:13 +0900

    dont know if works

--- PullDataFromWCA.py
+++ PullDataFromWCA.py
@@ -3,6 +3,8 @@ import pandas as pd
 wcaId = str(input())
 url = 'https://www.worldcubeassociation.org/persons/'+wcaId
 
+print(url)
+
 numOfComps = 0
 bestAverage3 = 0
 
@@ -11,8 +13,14 @@ print(dfs[1])
 print(dfs[1][['Average']].head())
 
 
-
+'''
 df2 = pd.DataFrame(data=dfs[1], index=['3x3x3 Cube'], columns=['Average'])
 print(df2)
 bestAverage3 = df2.loc['3x3x3 Cube']['Average']
 print(bestAverage3)
+'''
+'''
+df = dfs[1][['Average']].head()
+df.head()
+'''
+print(dfs[dfs.Event == '3x3x3 Cube'])
