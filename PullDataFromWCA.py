e57b6bf9bb4395878c70fc0725dfff0c1dced361
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-11-07 19:19:11 +0900

    import csv and search times

--- PullDataFromWCA.py
+++ PullDataFromWCA.py
@@ -1,20 +1,34 @@
 import pandas as pd
+import csv
+import pprint
 
-wcaId = str(input())
-url = 'https://www.worldcubeassociation.org/persons/'+wcaId
+registration = []
 
-#print(url)
+with open('RegistrationExportedFromWCACompPage.csv', newline='', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        registration.append(row)
 
-numOfComps = 0
-bestAverage3 = 0
+wcaidcol = 0
+for i in range(len(registration[0])):
+    if registration[0][i] == 'WCA ID':
+        wcaidcol = i
 
-dfs = pd.read_html(url)
 
-tmp = list(dfs[1][dfs[1]['Event'] == '3x3x3 Cube']['Average'])
+for i in range(1,len(registration)):
+    wcaId = registration[i][wcaidcol]
+    if wcaId != '':
+        url = 'https://www.worldcubeassociation.org/persons/'+wcaId
+        numOfComps = 0
+        bestAverage3 = 0
 
-if len(tmp) > 0:
-    bestAverage3 = tmp[0]
-else:
-    bestAverage3 = 0
+        dfs = pd.read_html(url)
 
-print(bestAverage3)
+        tmp = list(dfs[1][dfs[1]['Event'] == '3x3x3 Cube']['Average'])
+
+        if len(tmp) > 0:
+            bestAverage3 = tmp[0]
+        else:
+            bestAverage3 = 0
+
+        print(wcaId, bestAverage3)
