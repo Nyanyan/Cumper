33733c5409337a892b7ab8859b453176828bcea1
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-11-07 19:08:18 +0900

    modified program

--- PullDataFromWCA.py
+++ PullDataFromWCA.py
@@ -10,7 +10,11 @@ bestAverage3 = 0
 
 dfs = pd.read_html(url)
 
-print(dfs[0])
+tmp = list(dfs[1][dfs[1]['Event'] == '3x3x3 Cube']['Average'])
 
-bestAverage3 = dfs[1].at[0, 'Average']
+if len(tmp) > 0:
+    bestAverage3 = tmp[0]
+else:
+    bestAverage3 = 0
+
 print(bestAverage3)
